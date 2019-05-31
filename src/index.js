a0d241be729afacc28d30c0a00ded63257097dca
Luis Deschamps Rudge <luis@luisrudge.net>
2019-05-31 14:54:57 -0500

    inital setup

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider, createClient } from "urql";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import "./index.css";
+import App from "./App";
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const client = createClient({
+  url: "http://localhost:4000/graphql"
+});
+
+ReactDOM.render(
+  <Provider client={client}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
